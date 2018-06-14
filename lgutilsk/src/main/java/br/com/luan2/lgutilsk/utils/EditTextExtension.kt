a73b22a6d53af59913a8a45b8181234ba64f83d0
Luan Silva <luan@squarebits.com.br>
2018-06-14 17:12:04 -0300

    bug fixes

--- lgutilsk/src/main/java/br/com/luan2/lgutilsk/utils/EditTextExtension.kt
+++ lgutilsk/src/main/java/br/com/luan2/lgutilsk/utils/EditTextExtension.kt
@@ -9,6 +9,7 @@ import br.com.luan2.lgutilsk.extras.mask.CpfCnpjMask
 import br.com.luan2.lgutilsk.extras.mask.MyMaskEditText
 import br.com.luan2.lgutilsk.extras.mask.SuperBrazilianTelephoneMask
 import java.util.regex.Pattern
+
 /**
  * Created by luan gabriel on 16/04/18.
  */
@@ -27,19 +28,47 @@ fun EditText.shakeView() {
     this.startAnimation(shake)
 }
 
-fun EditText.isPasswordValid(maxLenght: Int): Boolean =  this.textTrim().length > maxLenght
+fun EditText.isPasswordValid(maxLenght: Int): Boolean = this.textTrim().length > maxLenght
 
-fun EditText.checkEmpty(editText: EditText): Boolean =  editText.text.trim().length < 0
+fun EditText.isEmpty(): Boolean = text.trim().length < 0
 
 
 fun EditText.checkEdittextError(error: String) {
     this.error = error
-    this.setFocusableInTouchMode(true)
+    this.isFocusableInTouchMode = true
     this.requestFocus()
     this.shakeView()
 }
 
-fun EditText.isEmailValid(): Boolean =  android.util.Patterns.EMAIL_ADDRESS.matcher(this.textTrim()).matches()
+fun checkEmptyMultiple(editTexts: Array<EditText>): Boolean {
+
+    for (currentField in editTexts) {
+        if (currentField.isEmpty()) {
+            return true
+        }
+    }
+    return false
+}
+
+fun checkEmptyMultipleWithError(editTexts: ArrayList<EditText>): Boolean {
+    var hasEmpty: Boolean = false
+    val wrongs: ArrayList<EditText> = ArrayList()
+    for (currentField in editTexts) {
+        if (currentField.isEmpty()) {
+            currentField.checkEdittextError("Esse campo não pode ser vazio!")
+            hasEmpty = true
+            wrongs.add(currentField)
+        }
+    }
+    if (wrongs.reversed().isNotEmpty()) {
+        wrongs[0].isFocusableInTouchMode = true
+        wrongs[0].requestFocus()
+    }
+    return hasEmpty
+}
+
+
+fun EditText.isEmailValid(): Boolean = android.util.Patterns.EMAIL_ADDRESS.matcher(this.textTrim()).matches()
 
 
 fun EditText.isLicensePlate(): Boolean {
@@ -55,10 +84,10 @@ fun EditText.validCep(): Boolean {
 }
 
 
-fun EditText.checkCep(completion: () -> Unit){
+fun EditText.checkCep(completion: () -> Unit) {
     this.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
         if (!hasFocus) {
-            if(this.validCep()){
+            if (this.validCep()) {
                 completion()
             }
         }
@@ -81,9 +110,11 @@ fun EditText.addCEPMask() = this.addTextChangedListener(MyMaskEditText(this, "##
 fun EditText.addPhoneMask() = this.addTextChangedListener(SuperBrazilianTelephoneMask(this))
 
 fun EditText.getDefaultValue(defaultValue: String): String {
-    return if (this.text.length > 0)
-        this.text.toString().trim { it <= ' ' }
+    return if (text.isNotEmpty())
+        text.toString().trim { it <= ' ' }
     else
         defaultValue
 
 }
+
+
